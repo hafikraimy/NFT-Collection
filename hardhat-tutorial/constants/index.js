b60336eed54eaed6d9b4a780ee18c0bec4b70686
hafikraimy <83526609+hafikraimy@users.noreply.github.com>
2022-06-18 22:40:17 +0800

    update metadata url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -2,6 +2,6 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xECeCD8597851EC2249816276D3A05556a95e8379";
 
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-henna-seven.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
